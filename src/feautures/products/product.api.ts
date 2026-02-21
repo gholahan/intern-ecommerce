0e9a8360608b5be3ab20aa8d4f02b10b5d40f334
Ibraheem <abdulazeezibraheem68@gmail.com>
2026-02-21 09:30:33 -0800

    feat(api): set up Axios client for app-wide use and refactor product API
    - Added central axios.ts with baseURL, JSON headers, and timeout
    - Refactored product API to destructure data and use Axios params
    - Fully typed with Product and ProductRes types for type safety

--- src/feautures/products/product.api.ts
+++ src/feautures/products/product.api.ts
@@ -1,28 +1,34 @@
-import axios from "axios";
 import type { Product } from "./types/product";
 import type { ProductRes } from "./types/productRes";
-
-const API_URL:string = 'https://dummyjson.com/products'
+import { api } from "../app/axios";
 
 export const fetchAllProduct = async (skip:number, limit:number):Promise<ProductRes>=>{
-  const res =await axios.get<ProductRes>(`${API_URL}?skip=${skip}&limit=${limit}`)
-  return res.data
+  const {data} = await api.get<ProductRes>("/products",{
+    params:{skip, limit}
+    })
+  return data
 };
 
 export const eachProduct = async (id:number):Promise<Product> => {
-  const res = await axios.get<Product>(`${API_URL}/${id}`);
-  return res.data
+  const {data} = await api.get<Product>(`/products/${id}`);
+  return data
 }
-export const getSearch = async (search:string,skip:number, limit:number):Promise<ProductRes> => {
-   const res =await axios.get<ProductRes>(`https://dummyjson.com/products/search?q=${search}&skip=${skip}&limit=${limit}`)
-   return res.data
+
+export const getSearch = async (search:string, skip:number, limit:number):Promise<ProductRes> => {
+   const {data} = await api.get<ProductRes>("/products/search",{
+    params:{q:search, skip, limit}
+   });
+   return data
 }
-export const getProductByCategory = async (category:string,skip:number, limit:number):Promise<ProductRes> => {
-   const res =await axios.get<ProductRes>(`https://dummyjson.com/products/category/${category}?skip=${skip}&limit=${limit}`)
-   return res.data
+
+export const getProductByCategory = async (category:string, skip:number, limit:number):Promise<ProductRes> => {
+   const {data} = await api.get<ProductRes>(`/products/category/${category}`,{
+    params:{skip, limit}
+   });
+   return data
 }
 
 export const getCategories = async (): Promise<string[]> => {
-  const res = await axios.get<string[]>("https://dummyjson.com/products/category-list");
-  return res.data; // directly the array of strings
+  const { data } = await api.get<string[]>('/products/category-list');
+  return data;
 };
