ebbba32b7663313325e039b198337ff6f32ced22
Ibraheem <abdulazeezibraheem68@gmail.com>
2026-03-04 23:42:25 -0800

    refactor :- -implemented RBAC -type user properly -useUser query now depends on persisted refreshToken for proper page mount and referesh flow

--- src/feautures/app/routes/ProtectedRoute.tsx
+++ src/feautures/app/routes/ProtectedRoute.tsx
@@ -1,14 +1,19 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../../auth/useUser";
 import Authloader from "../../../shared/components/loaders/Authloader";
+import type { User } from "../../auth/type";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({accessRole} : {accessRole ?:User["role"]}) => {
   const { data: user, isLoading, isError } = useUser();
 
   if (isLoading) return <Authloader/>;
 
   if (isError || !user) {
     return <Navigate to="/login" replace />;
+  }
+
+  if(accessRole && user.role !== accessRole){
+    return <Navigate to="/" replace/>
   }
 
   return <Outlet />;
--- src/feautures/auth/useUser.ts
+++ src/feautures/auth/useUser.ts
@@ -1,16 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { useAuthStore } from "./auth.store";
 import { getuserData } from "./auth.api";
+import type { User } from "./type";
 
 export const useUser = () => {
-  const accessToken = useAuthStore((s) => s.accessToken);
+  const refreshToken= useAuthStore((s) => s.refreshToken);
 
-  const query = useQuery({
-    queryKey: ['user', accessToken],
-    queryFn: getuserData,
-    enabled: !!accessToken,
-    staleTime: 1000 * 60 * 5,
-  });
+  const query = useQuery<User>({
+  queryKey: ['user', refreshToken],
+  queryFn: getuserData,
+  enabled: !!refreshToken,
+  staleTime: 1000 * 60 * 5,
+  gcTime: 1000 * 60 * 5,
+});
+
 
   return query;
 };
