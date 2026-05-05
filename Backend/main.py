be378413b874eb636df8efe9b71c37432fa80bfe
Ibraheem <abdulazeezibraheem68@gmail.com>
2026-05-05 01:11:44 -0700

    CORS fix test

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,7 @@ from checkout.routes.billing import router as billing_router
 from checkout.routes.webhook import router as webhook_router
 from checkout.routes.verifyPayment import router as verify_payment_router
 from fastapi.middleware.cors import CORSMiddleware
-from core.config import ORIGINS
+# from core.config import ORIGINS
 
 
 app = FastAPI(title="Exclusive e_commerce API")
@@ -13,15 +13,13 @@ app.include_router(verify_payment_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-    "https://e-commerce-webapp-riz3.vercel.app"
-],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-print("CORS ORIGINS:", ORIGINS)
+# print("CORS ORIGINS:", ORIGINS)
 
 @app.get("/")
 async def health_check():
--- Frontend/src/feautures/checkout/api/checkoutApi.ts
+++ Frontend/src/feautures/checkout/api/checkoutApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import type { OrderPayload, checkoutRes } from "../types";
 
-const checkoutApi = axios.create({
-  baseURL: import.meta.env.VITE_PAYMENTS_URL, // or payments URL if separate
+export const checkoutApi = axios.create({
+  baseURL: "https://e-commerce-webapp-uui3.onrender.com" // or payments URL if separate
 });
 
 export const postCheckout = async (payload:OrderPayload): Promise<checkoutRes> => {
